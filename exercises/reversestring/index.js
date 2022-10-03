75a9e23f16714f43ce1e7d1d2fef7f9920a31828
Cody Koscielski <codykoscielski@gmail.com>
2022-10-03 08:20:19 -0400

    Reversed string solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,8 +6,20 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+//JavaScipt way without using loops
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+//Using a for loop
 function reverse(str) {
-    console.log(...str)
+    let reversed = ''
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i]
+    }
+
+    return reversed
 }
 
 module.exports = reverse;
